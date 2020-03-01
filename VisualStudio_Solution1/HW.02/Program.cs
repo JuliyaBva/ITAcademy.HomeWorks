9b55083deeea1d1bcab01d48825957d6de1e0aba
JuliyaBva <yuliyab8508@gmail.com>
2020-03-01 23:30:46 +0300

    add steps with code comments

---
+++ VisualStudio_Solution1/HW.02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Create an instance of the StreamReader class to read information from a file located by the specified way and save it as textReader variable.
+            //2. Read information from textReader by method ReadToEnd and returns it as string textReaderResult variable.
+            //3. Release all resources used by textReader by method Dispose.
+            //4. Split textReaderResult and save it as string[] arrayOfTextResult variable.
+            //5. Create byte[] imageBytes array with definition of the array length (length minus one element).
+            //6. Initialization of the counter; continuation condition; iteration.
+            //7. Convert a specified value (arrayOfTextResult[i], 2) to an 8-bit unsigned integer and save it to byte binary variable.
+            //8. Assignment binary to imageBytes[i].
+            //9. Create a new file (image.bmp), writes the specified byte array (imageBytes) to the file, and then closes the file. 
+        }
+    }
+}
